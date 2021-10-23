68a74a6e6d92348d9d4f31f9bb879b3ea2521fbf
sreelakshmi <pannikuzhiyilputhenparampiloommen.1877752@studenti.uniroma1.it>
2021-10-23 19:47:00 +0200

    Updated Application file

--- Complex.cpp
+++ Complex.cpp
@@ -1,11 +1,24 @@
 //Complex.cpp
-#include "Datum.h"
+#include "Complex.h"
 
 int main(){
 
-  Complex z(3,5);
-  z.real_part();
-  z.imag_part();
+  Complex z(3.15,-5.8);
+  z.print("z");
+  cout<<"Real Part ="<<z.real_part()<<endl;
+  cout<<"Imaginary Part = "<<z.imag_part()<<endl;
+  cout<< "Magnitude = "<<z.mag()<<endl;
+  cout<<"Phase = "<<z.phase()<< endl;
 
+
+  
+  z.setReal(3);
+  z.setImag(4);
+  z.print("new z ");
+  cout<<"Real Part ="<<z.real_part()<<endl;
+  cout<<"Imaginary Part = "<<z.imag_part()<<endl;
+  cout<< "Magnitude = "<<z.mag()<<endl;
+  cout<<"Phase = "<<z.phase()<< endl;
+  
   return 0;
 }
