5cf473477d358ded071f0b0383f0ee36fc60be57
sreelakshmi <pannikuzhiyilputhenparampiloommen.1877752@studenti.uniroma1.it>
2021-10-24 00:19:53 +0200

    Updated Header file with overloading of operators and global fuunctions

--- Complex.h
+++ Complex.h
@@ -1,6 +1,6 @@
 //Complex.h
-//#ifndef Complex_h
-//#define Complex_h
+#ifndef Complex_h
+#define Complex_h
 
 #include<iostream>
 using std::cout;
@@ -14,7 +14,8 @@ class Complex{
   Complex();
   //Regular Constructor
   Complex(double real, double imag);
-  
+  //Conjugate
+  Complex conj();
   //Getter Method
   //Real Part
   double real_part()  const {return real_;}
@@ -28,6 +29,25 @@ class Complex{
   //Setter Method
   void setReal(double real){real_ = real;}
   void setImag(double imag){imag_=imag;}
+
+  //Overloading Operators +,-,*,/ respectively
+
+  Complex operator+ (const Complex& rhs) const;
+  Complex operator- (const Complex& rhs) const;
+  Complex operator* (const Complex& rhs) const;
+  Complex operator/ (const Complex& rhs) const;
+
+  //Interaction between Complex and double object
+  Complex operator+(const double& rhs) const;
+  Complex operator-(const double& rhs) const;
+  Complex operator*(const double& rhs) const;
+  Complex operator/(const double& rhs) const;
+
+  //using friend method to gloabl functions to access private data members
+  friend Complex operator+(const double& lhs, const Complex& rhs);
+  friend Complex operator-(const double& lhs, const Complex& rhs);
+  friend Complex operator*(const double& lhs, const Complex& rhs);
+  friend Complex operator/(const double& lhs, const Complex& rhs);
   
   //utility fuction
   void print(const std::string& complexnumber) const;
@@ -36,3 +56,12 @@ private:
   double real_;
   double imag_;
 };
+
+//we have to achieve symetry between Complex and double ie, Complex*double and double*complex
+//Complex*double is already achieved. Because we can overload operator of type Complex.
+//But  double*Complex means we need an operator of type double to  be overloaded. But we cannot overload built-in types
+//So this s achieved by Global Function
+//Global Function is declared in the Header file, outside the class scope and Implemented in Source File
+
+//Complex operator+(const doube& lhs, const Complex& rhs);
+#endif
